1eb621d9c78cbaa34e908e3dc4d306b5991db9b4
RomaSkrund <Forest12321@yandex.ru>
2023-08-18 12:53:41 +0300

    Основные файлы проекта
    Изначальная версия, которая будет дорабатываться

---
+++ Assets/Scripts/AutoCarCreate.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Xml.Linq;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+
+public class AutoCarCreate : MonoBehaviour
+{
+    [NonSerialized]
+    public bool IsEnemy = false; 
+    public GameObject car;
+    public float time = 5f;
+    public int countOfNewCars = 3;
+    public Material enemyHealthBar;
+
+    private void Start()
+    {
+        StartCoroutine(SpawnCar());
+    }
+
+    IEnumerator SpawnCar()
+    {
+        for (int i = 1; i <= countOfNewCars; i++)
+        {
+            yield return new WaitForSeconds(time);
+
+            Vector3 pos = new Vector3(
+                transform.GetChild(0).position.x + UnityEngine.Random.Range(3f, 7f),
+                transform.GetChild(0).position.y,
+                transform.GetChild(0).position.z + UnityEngine.Random.Range(3f, 7f)); // исправить 
+
+            GameObject spawn = Instantiate(car, pos, Quaternion.identity);
+
+            if (IsEnemy)
+            {
+                spawn.tag = "Enemy";
+                spawn.transform.GetChild(0).gameObject.GetComponent<Renderer>().material = enemyHealthBar;
+            }
+        }
+    }
+}
